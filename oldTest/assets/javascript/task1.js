dc84f7680bcb7af9dbfb01f9d1a5aaef3601afbc
Uriahx <uriah.squarcina@vtsd.com>
2024-01-18 11:46:04 -0500

    boh3

--- oldTest/assets/javascript/task1.js
+++ oldTest/assets/javascript/task1.js
@@ -1,1 +1,9 @@
 /* Put code for the task below */
+$(document).ready(function(){
+    $("#imageTask1").mouseenter(function(){
+        $("#buttonTask1").show();
+    });
+    $("#buttonTask1").click(function(){
+        $("#imageTask1").hide();
+    });
+});
